dd522e1c2007716fed7c7c4811018abacc28b675
jgwq10 <jgrochocinska@edu.cdv.pl>
2025-11-28 23:37:40 +0100

    Add files via upload

---
+++ MiniProjekt/MiniProjekt/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+
+class Pojazd
+{
+    public virtual void Start() => Console.WriteLine("Pojazd został uruchomiony");
+}
+
+class Samochod : Pojazd
+{
+    public void Jedz() => Console.WriteLine("Samochód jedzie");
+}
+
+class ElektrycznySamochod : Samochod
+{
+    public void Laduj() => Console.WriteLine("Ładowanie baterii");
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       
+        Pojazd pojazd = new Pojazd();
+        pojazd.Start();
+        Console.WriteLine();
+
+        Samochod samochod = new Samochod();
+        samochod.Start(); 
+        samochod.Jedz();
+        Console.WriteLine();
+
+        ElektrycznySamochod eSamochod = new ElektrycznySamochod();
+        eSamochod.Start(); 
+        eSamochod.Jedz();  
+        eSamochod.Laduj(); 
+    }
+}
