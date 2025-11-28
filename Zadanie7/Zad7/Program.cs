2617519b232790f397f8ddbee175b86e059df2bb
jgwq10 <jgrochocinska@edu.cdv.pl>
2025-11-28 23:40:01 +0100

    Add files via upload

---
+++ Zadanie7/Zad7/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Zwierze
+{
+    public virtual void DajGlos() => Console.WriteLine("Zwierzę wydaje odgłos");
+}
+
+class Pies : Zwierze
+{
+    public override void DajGlos() => Console.WriteLine("Hau hau!");
+}
+
+class Kot : Zwierze
+{
+    public override void DajGlos() => Console.WriteLine("Meow!");
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Zwierze[] zwierzeta = new Zwierze[]
+        {
+            new Pies(),
+            new Kot(),
+            new Zwierze(),
+            new Kot(),
+            new Pies()
+        };
+
+        foreach (Zwierze z in zwierzeta)
+        {
+            z.DajGlos();
+        }
+    }
+}
